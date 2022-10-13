c66c7c5be699bc4d49d7d9c280362a71cb0c4567
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-10-13 11:38:08 +0700

    integrating viewmodel

--- app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.android.unscramble.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.android.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,6 +19,9 @@ class GameViewModel: ViewModel() {
     private lateinit var currentWord: String
     // Set of words used in the game
     private var usedWords: MutableSet<String> = mutableSetOf()
+
+    var userGuess by mutableStateOf("")
+        private set
 
     private fun pickRandomWordAndShuffle(): String {
         // Continue picking up a new random word until you get one that hasn't been used before
@@ -41,6 +47,10 @@ class GameViewModel: ViewModel() {
     fun resetGame() {
         usedWords.clear()
         _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+
+    fun updateUserGuess(guessedWord: String){
+        userGuess = guessedWord
     }
 
     init {
